b5a72631b947c5e53e9a19906ce53105399fabce
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-13 16:11:23 -0300

    add delete at sidedish controller

--- src/sidedishe/controller/sidedish.ts
+++ src/sidedishe/controller/sidedish.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { SideDish } from 'src/database/models/sidedish';
 import { SideDishService } from '../service/sidedish';
 
@@ -14,5 +14,10 @@ export class SidedishController {
   @Get()
   async findAll(): Promise<SideDish[]> {
     return await this.sideDishService.findAll();
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<void> {
+    return await this.sideDishService.delete(id);
   }
 }
