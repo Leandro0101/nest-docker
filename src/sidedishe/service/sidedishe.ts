a7d804247864f98427e64aeefb21b30041398394
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-12 22:06:59 -0300

    added create at sidedishe services

--- src/sidedishe/service/sidedishe.ts
+++ src/sidedishe/service/sidedishe.ts
@@ -5,4 +5,9 @@ import { SideDishe } from 'src/database/models/sidedishe';
 @Injectable()
 export class SideDisheService {
   constructor(@InjectModel('SideDishe') private sideDisheModel: SideDishe) {}
+
+  async create(sidedishe: SideDishe): Promise<SideDishe> {
+    const createdSideDishe = new SideDishe(sidedishe);
+    return await createdSideDishe.save();
+  }
 }
