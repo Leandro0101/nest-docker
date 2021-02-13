ea8635bac0eb6de8da4ad7252a480f99ae440307
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-13 17:25:02 -0300

    add create at address controller

--- src/address/controller/address.ts
+++ src/address/controller/address.ts
@@ -2,7 +2,12 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { Address } from 'src/database/models/address';
 import { AddressService } from '../service/address';
 
-@Controller()
+@Controller('adresses')
 export class AddressController {
   constructor(private addressService: AddressService) {}
+
+  @Post()
+  async create(@Body() address: Address): Promise<Address> {
+    return await this.addressService.create(address);
+  }
 }
