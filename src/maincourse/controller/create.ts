80e42e7ea03213ab1e637d9187cd4659e381c79f
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-16 13:58:02 -0300

    refact CreateMainCourseController

--- src/maincourse/controller/create.ts
+++ src/maincourse/controller/create.ts
@@ -1,28 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { MainCourse } from 'src/database/models/maincourse';
-import { MainCourseService } from '../service/implementations/maincourse';
+import { CreateMainCourseService } from '../service/create';
 
 @Controller('maindish')
-export class MainCourseController {
-  constructor(private mainCourseService: MainCourseService) {}
+export class CreateMainCourseController {
+  constructor(private createMainCourseService: CreateMainCourseService) {}
 
   @Post()
   async create(@Body() mainCourse: MainCourse): Promise<MainCourse> {
-    return await this.mainCourseService.create(mainCourse);
-  }
-
-  @Get()
-  async findAll(): Promise<MainCourse[]> {
-    return await this.mainCourseService.findAll();
-  }
-
-  @Delete(':id')
-  async delete(@Param('id') id: string): Promise<void> {
-    return await this.mainCourseService.delete(id);
-  }
-
-  @Get(':id')
-  async findById(@Param('id') id: string): Promise<MainCourse> {
-    return this.mainCourseService.findById(id);
+    const createdMainCourse: MainCourse = await this.createMainCourseService.create(
+      mainCourse,
+    );
+    return createdMainCourse;
   }
 }
