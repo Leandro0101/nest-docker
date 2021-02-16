36efa35b5d73d7a17fe6b7a6fec4ec217c6f0812
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-16 19:28:02 -0300

    add FindAllSideDishByIdController

---
+++ src/sidedish/controller/findAll.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { SideDish } from 'src/database/models/sidedish';
+import { FindAllSideDishService } from '../service/findAll';
+
+@Controller('sidedishes')
+export class FindAllSidedishController {
+  constructor(private findAllCreateSideDishService: FindAllSideDishService) {}
+
+  @Get()
+  async findAll(): Promise<SideDish[]> {
+    const foundedsSideDishes: SideDish[] = await this.findAllCreateSideDishService.findAll();
+    return foundedsSideDishes;
+  }
+}
