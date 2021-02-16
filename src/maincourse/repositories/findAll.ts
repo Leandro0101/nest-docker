68f5687b71a5ce9896edf676415d762d8bf20e45
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-16 13:40:58 -0300

    add FindAllMainCourseRepository

--- src/maincourse/repositories/findAll.ts
+++ src/maincourse/repositories/findAll.ts
@@ -1,16 +1,14 @@
-import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { MainCourse } from 'src/database/models/maincourse';
 
-@Injectable()
-export class FindAllMainCourseService {
+export class DeleteMainCourseRepository {
   constructor(
     @InjectModel('MainCourse')
     private readonly mainCourseModel: Model<MainCourse>,
   ) {}
 
-  async findAll(): Promise<MainCourse[]> {
+  async exec(): Promise<MainCourse[]> {
     return this.mainCourseModel.find().exec();
   }
 }
