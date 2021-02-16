7d244c2caa09234055774667a8df4678a2873fc1
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-16 13:30:53 -0300

    refact CreateMainCourseService

--- src/maincourse/service/implementations/create.ts
+++ src/maincourse/service/implementations/create.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { MainCourse } from 'src/database/models/maincourse';
-import { ICreateMainCourse } from '../create';
+import { CreateMainCourseRepository } from 'src/maincourse/repositories/create';
 
 @Injectable()
-export class CreateMainCourseService implements ICreateMainCourse {
+export class CreateMainCourseService {
   constructor(
-    @InjectModel('MainCourse')
-    private readonly mainCourseModel: Model<MainCourse>,
+    private readonly createMainCourseRepository: CreateMainCourseRepository,
   ) {}
 
   async create(mainCourse: MainCourse): Promise<MainCourse> {
-    const createdMainCourse = new this.mainCourseModel(mainCourse);
-    return createdMainCourse.save();
+    const createdMainCourse: MainCourse = await this.createMainCourseRepository.exec(
+      mainCourse,
+    );
+
+    return createdMainCourse;
   }
 }
