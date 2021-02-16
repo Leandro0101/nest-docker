35be00acf151188be252c0b5b5237e157983a079
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-16 13:39:14 -0300

    refact DeleteMainCourseService

--- src/maincourse/service/implementations/delete.ts
+++ src/maincourse/service/implementations/delete.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { MainCourse } from 'src/database/models/maincourse';
-import { IDeleteMainCourse } from '../delete';
+import { DeleteMainCourseRepository } from 'src/maincourse/repositories/delete';
 
 @Injectable()
-export class DeleteMainCourseService implements IDeleteMainCourse {
+export class DeleteMainCourseService {
   constructor(
-    @InjectModel('MainCourse')
-    private readonly mainCourseModel: Model<MainCourse>,
+    private readonly deleteMainCourseRepository: DeleteMainCourseRepository,
   ) {}
 
   async delete(id: string): Promise<void> {
-    return await this.mainCourseModel.deleteOne({ _id: id }).exec();
+    await this.deleteMainCourseRepository.exec(id);
   }
 }
