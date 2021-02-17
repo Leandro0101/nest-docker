3d204133a8bbbecb299cc839f1011aeb2ada735b
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-16 21:45:49 -0300

    add FindAllAddressRepository

--- src/address/repositories/findAll.ts
+++ src/address/repositories/findAll.ts
@@ -2,14 +2,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Address } from 'src/database/models/address';
 
-export class CreateAddressRepository {
+export class FindAllAddressRepository {
   constructor(
     @InjectModel('Address')
     private readonly addressModel: Model<Address>,
   ) {}
 
-  async exec(address: Address): Promise<Address> {
-    const createdAddress = new this.addressModel(address);
-    return await createdAddress.save();
+  async exec(): Promise<Address[]> {
+    const foundedsAdresses: Address[] = await this.addressModel.find().exec();
+
+    return foundedsAdresses;
   }
 }
