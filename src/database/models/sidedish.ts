6f6fef278ffda6db399e5ca49f49d39d2b55abea
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-17 22:30:11 -0300

    add ISideDish interface

--- src/database/models/sidedish.ts
+++ src/database/models/sidedish.ts
@@ -1,10 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { ISideDish } from 'src/shared/protocols/sidedish';
 
 export type SideDishDocument = SideDish & Document;
 
 @Schema()
-export class SideDish extends Document {
+export class SideDish extends Document implements ISideDish {
   @Prop()
   name: string;
 
---
+++ src/shared/datasource/sidedishDatasource.ts
@@ -0,0 +1,16 @@
+import { ISideDish } from '../protocols/sidedish';
+
+export class SideDishDataSource {
+  private sidedishes: ISideDish[] = [
+    { name: 'sidedish 1', price: 25 },
+    { name: 'sidedish 2', price: 15 },
+    { name: 'sidedish 3', price: 25 },
+    { name: 'sidedish 4', price: 23 },
+    { name: 'sidedish 5', price: 50 },
+  ];
+
+  findOne(index: number): ISideDish {
+    const sideDish: ISideDish = this.sidedishes[index];
+    return sideDish;
+  }
+}
